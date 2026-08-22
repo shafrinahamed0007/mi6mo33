4d2fe3a02d3eed750a3e4977296d9cca2bbce8a4
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-08-22 22:21:23 +0600

    Show flags, and handle data error -  33-4

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,12 @@
 
 
-const Country = () => {
+const Country = ({country}) => {
+    
     return (
         <div>
-            
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+            <h4>Name: {country?.name?.common}</h4>
+            <h3>Population: {country?.population?.population}</h3>
         </div>
     );
 };
