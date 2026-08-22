c31f09905ab12e008b21ff131edb16b266dd3fd9
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-08-22 17:29:54 +0600

    display countries in a simple way

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-
-import './App.css'
-import Countries from './components/Countries/Countries'
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./components/Countries/Countries";
 
 function App() {
-
+  const countriesPromise = fetch(
+    "https://openapi.programming-hero.com/api/all",
+  ).then((res) => res.json());
 
   return (
     <>
       <section id="root">
-        
         <div>
           <h1>React World on the go...</h1>
-          <Countries />
-         
+          <Suspense fallback={<p>Countries data loadng...</p>}>
+            <Countries countriesPromise={countriesPromise} />
+          </Suspense>
         </div>
-        
       </section>
-
-
-
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,19 @@
+import { use } from "react";
+import Country from "../Country/Country";
 
-
-const Countries = () => {
-    return (
-        <div>
-            <h3>In the Countries</h3>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  // console.log("All Countries: ", countries)
+  return (
+    <div>
+      <h3>In the Countries</h3>
+      <h4>Total Countires: {countries.length}</h4>
+      {countries.map((country) => (
+        <Country key={country.id} country={country} />
+      ))}
+    </div>
+  );
 };
 
 export default Countries;
